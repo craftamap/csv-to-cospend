c3c0f4a6a872032b7c9ac191b547998af350239a
Fabian Siegel <fabians1999@gmail.com>
2023-07-08 12:47:24 +0200

    refactored using typer and to use functions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import typer
+from typing_extensions import Annotated
 import sys
 import datetime
 import csv
@@ -10,10 +12,9 @@ import requests
 from decimal import Decimal
 import dataclasses
 from dataclasses import dataclass
+from pathlib import Path
 
 config = toml.load("config.toml")
-columns = config["csv"]["columns"]
-
 
 @dataclass
 class Payment:
@@ -41,43 +42,36 @@ class Payment:
         )
 
 
-payments = []
+app = typer.Typer()
 
-with open(sys.argv[1]) as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=";")
-    for row in reader:
-        date = datetime.datetime.strptime(row[columns["date"]], "%d.%m.%Y").date()
-        # replace commas with dots
-        # parse the number using Decimal to not loose any data through double precision
-        # take it times 100 to get to the cents
-        # invert it for cospend and convert it to an int
-        amount = int(-(Decimal(row[columns["amount"]].replace(",", ".")) * 100))
+def csv_to_payment(csv_path: Path) -> list[Payment]:
+    columns = config["csv"]["columns"]
+    payments = []
 
-        payments.append(
-            Payment(
-                date=date,
-                payee=row[columns["payee"]],
-                payee_friendly=row[columns["payee"]],
-                reference=row[columns["reference"]],
-                amount=amount,
-                category=None,
+    with open(sys.argv[1]) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";")
+        for row in reader:
+            date = datetime.datetime.strptime(row[columns["date"]], "%d.%m.%Y").date()
+            # replace commas with dots
+            # parse the number using Decimal to not loose any data through double precision
+            # take it times 100 to get to the cents
+            # invert it for cospend and convert it to an int
+            amount = int(-(Decimal(row[columns["amount"]].replace(",", ".")) * 100))
+
+            payments.append(
+                Payment(
+                    date=date,
+                    payee=row[columns["payee"]],
+                    payee_friendly=row[columns["payee"]],
+                    reference=row[columns["reference"]],
+                    amount=amount,
+                    category=None,
+                )
             )
-        )
+    return payments
 
 
-results: dict[str, list[Payment]] = {
-    "approved": [],
-    "second_look": [],
-    "ignore": [],
-}
-
-items = list(reversed(payments))
-exit_loop = False
-for idx, payment in enumerate(items):
-    if exit_loop:
-        break
-    print()
-    print()
+def simplify_payment(payment: Payment) -> Payment:
     for rule in config["naming"]["rule"]:
         if not rule.get("payee_contains") is None:
             if rule.get("payee_contains").lower() in payment.payee.lower():
@@ -95,57 +89,9 @@ for idx, payment in enumerate(items):
                     payee_friendly=rule["result"].get("name", payment.payee),
                     category=rule["result"].get("category", payment.category),
                 )
-
-    print(f"{idx+1}/{len(payments)}")
-    print(f"Date:       {payment.date.isoformat()}")
-    print(f"Payee:      {payment.payee_friendly} ({payment.payee})")
-    print(f"Reference:  {payment.reference}")
-    print(f"Amount:     {payment.amount/100:.2f} €")
-
-    while True:
-        print(
-            "(a) approved / (c) add category / (e) edit by hand and approve / (j) second look / (x) ignore / (q) quit"
-        )
-        entered = input()
-        if entered == "a":
-            results["approved"].append(payment)
-            print("added to the approved list")
-            break
-        if entered == "c":
-            print("(g) grocery")
-            print("(s) shopping")
-            print("(x) dont change")
-            categories = {
-                "g": "grocery",
-                "s": "shopping",
-            }
-            payment.category = categories.get(input())
-        elif entered == "e":
-            with tempfile.NamedTemporaryFile("w", delete=False) as tmpF:
-                js = json.dump(payment.to_json(), tmpF, indent=2)
-                path = tmpF.name
-            os.system("%s %s" % (os.getenv("EDITOR"), path))
-            with open(path) as tmpF:
-                payment = Payment.from_json(json.load(tmpF))
-            results["approved"].append(payment)
-            print("added to the approved list")
-            break
-        elif entered == "j":
-            results["second_look"].append(payment)
-            print("added to the second look list")
-            break
-        elif entered == "x":
-            results["ignore"].append(payment)
-            print("added to the ignore list")
-            break
-        elif entered == "q":
-            results["second_look"].extend(items[idx:])
-            print("adding all remaining items to second look list")
-            exit_loop = True
-            break
+    return payment
 
 
-# let's persist all lists first
 def persist(items: list[Payment], path: str):
     print("dumping to ", path)
     with open(path, "w") as fh:
@@ -153,11 +99,79 @@ def persist(items: list[Payment], path: str):
     print("done")
 
 
-persist(results["approved"], f"{datetime.datetime.now().isoformat()}-approved.json")
-persist(
-    results["second_look"], f"{datetime.datetime.now().isoformat()}-second_look.json"
-)
-persist(results["ignore"], f"{datetime.datetime.now().isoformat()}-ignore.json")
+def classify_payments(
+    payment: Payment,
+) -> (list[Payment], list[Payment], list[Payment]):
+    approved: list[Payment] = []
+    second_look: list[Payment] = []
+    ignore: list[Payment] = []
+
+    items = list(reversed(payments))
+    exit_loop = False
+    for idx, payment in enumerate(items):
+        if exit_loop:
+            break
+        print()
+        payment = simplify_payment(payment)
+
+        print(f"{idx+1}/{len(payments)}")
+        print(f"Date:       {payment.date.isoformat()}")
+        print(f"Payee:      {payment.payee_friendly} ({payment.payee})")
+        print(f"Reference:  {payment.reference}")
+        print(f"Amount:     {payment.amount/100:.2f} €")
+
+        while True:
+            print(
+                "(a) approved / (c) add category / (e) edit by hand and approve / (j) second look / (x) ignore / (q) quit"
+            )
+            entered = input()
+            if entered == "a":
+                approved.append(payment)
+                print("added to the approved list")
+                break
+            if entered == "c":
+                print("(g) grocery")
+                print("(s) shopping")
+                print("(x) dont change")
+                categories = {
+                    "g": "grocery",
+                    "s": "shopping",
+                }
+                payment.category = categories.get(input())
+            elif entered == "e":
+                with tempfile.NamedTemporaryFile("w", delete=False) as tmpF:
+                    js = json.dump(payment.to_json(), tmpF, indent=2)
+                    path = tmpF.name
+                os.system("%s %s" % (os.getenv("EDITOR"), path))
+                with open(path) as tmpF:
+                    payment = Payment.from_json(json.load(tmpF))
+                approved.append(payment)
+                print("added to the approved list")
+                break
+            elif entered == "j":
+                second_look.append(payment)
+                print("added to the second look list")
+                break
+            elif entered == "x":
+                ignore.append(payment)
+                print("added to the ignore list")
+                break
+            elif entered == "q":
+                second_look.extend(items[idx:])
+                print("adding all remaining items to second look list")
+                exit_loop = True
+                break
+    return []
+
+
+@app.command()
+def classify(csv_path: Annotated[Path, typer.Argument()]):
+    payments = csv_to_payment(csv_path)
+    (approved, second_look, ignore) = classify_payments(payments)
+
+    persist(approved, f"{datetime.datetime.now().isoformat()}-approved.json")
+    persist(second_look, f"{datetime.datetime.now().isoformat()}-second_look.json")
+    persist(ignore, f"{datetime.datetime.now().isoformat()}-ignore.json")
 
 
 def create_bill(payment: Payment):
@@ -181,6 +195,14 @@ def create_bill(payment: Payment):
     print(response.status_code, response.text)
 
 
-for payment in results["approved"]:
-    print("create_bill", payment.payee)
-    create_bill(payment)
+@app.command()
+def push(json_path: Annotated[Path, typer.Argument()]):
+    with open(json_path) as tmpF:
+        payments = [Payment.from_json(x) for x in json.load(tmpF)]
+    for payment in payments:
+        print(f"posting {payment}")
+        create_bill(payment)
+
+
+if __name__ == "__main__":
+    app()
